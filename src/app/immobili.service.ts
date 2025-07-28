efd7a4bad86a34f517c0b90abe827c1879c7093f
Giuss2 <120112582+Giuss2@users.noreply.github.com>
2025-07-28 16:49:16 +0200

    prova

--- src/app/immobili.service.ts
+++ src/app/immobili.service.ts
@@ -3,12 +3,14 @@ import { Immobile } from '../../data';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { map, catchError, EMPTY } from 'rxjs';
 import Instant from 'ts-time/Instant';
+import { AuthService } from '../app/_services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImmobiliService {
   private http = inject(HttpClient);
+  private authService = inject(AuthService);
 
   constructor(http: HttpClient, auth: AuthService) {
     this.http = http;
