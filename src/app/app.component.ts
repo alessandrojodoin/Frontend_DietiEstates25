0207f1c81024d1fcd38284bf5dc0ae510477eef4
Giuss2 <120112582+Giuss2@users.noreply.github.com>
2025-09-17 12:17:20 +0200

    working on riepilogo-attivita

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { RiepilogoAttivitaComponent } from "./riepilogo-attivita/riepilogo-attivita.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, RiepilogoAttivitaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
