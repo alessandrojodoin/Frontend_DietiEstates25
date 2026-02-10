5a4d7f2fde7ba2ef6a5a5fec9c4817d4c63628d7
Giuss2 <giusymarino2015@gmail.com>
2026-02-10 20:22:50 +0100

    google calendar con iframe (no notifiche)

--- src/app/dashboard-agente/prenotazioni-ricevute/prenotazioni-ricevute.component.ts
+++ src/app/dashboard-agente/prenotazioni-ricevute/prenotazioni-ricevute.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../_services/auth.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-prenotazioni-ricevute',
   standalone: true,
-  imports: [],
-  templateUrl: './prenotazioni-ricevute.component.html',
-  styleUrl: './prenotazioni-ricevute.component.scss'
+  imports: [CommonModule],
+  templateUrl: './prenotazioni-ricevute.component.html'
 })
 export class PrenotazioniRicevuteComponent {
+  public auth = inject(AuthService);
+  private sanitizer = inject(DomSanitizer);
 
+  calendarUrl!: SafeResourceUrl;
+
+  ngOnInit() {
+    
+    const calendarId = 'progettouniversita2025@gmail.com';
+    this.calendarUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      `https://calendar.google.com/calendar/embed?src=${encodeURIComponent(calendarId)}&ctz=Europe/Rome`
+    );
+  }
 }
