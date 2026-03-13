e7e804adf88de010cf267f87abb694b4abac541d
Alessandro Jodoin <a.jodoin@studenti.unina.it>
2026-03-13 13:12:30 +0100

    Error Message Fixes

--- PushContainerImage.cmd
+++ PushContainerImage.cmd
@@ -1,4 +1,4 @@
-
+call az acr login --name DietiEstatesRESTAPI
 docker image build -t frontend-dietiestates25 .
 docker tag frontend-dietiestates25 dietiestatesrestapi.azurecr.io/frontend-dietiestates25
 docker push dietiestatesrestapi.azurecr.io/frontend-dietiestates25
